4f98b43999eb06f940ae479708f9bb9703d9b0f4
sivaprasadnk <sivaprasadnk123@gmail.com>
2025-05-20 17:22:32 +0530

    updating sdk

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\SP\sdk\flutter.2.10.4\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\SP\flutterProj\packages\un_published\double_tap_to_exit\double_tap_to_exit"
+export "FLUTTER_ROOT=E:\flutter sdk\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\sp\packages\double_tap_to_exit"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.2"
+export "FLUTTER_BUILD_NUMBER=1.0.2"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
